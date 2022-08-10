ec4237c34b8dc2640e48732871786fe6cdb4327a
+ <+>
2022-08-10 14:47:35 -0400

    commit tree 0aa8b2e9f1e1a65f3d44dcd4c108b684e3f33550

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,7 +3,7 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at]
   end
 
   # GET /cheeses/:id
